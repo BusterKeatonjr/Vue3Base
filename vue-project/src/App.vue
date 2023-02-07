205f38c039b451b6a8e783fdabc8940e8d813685
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-07 21:22:01 +0300

    Передача параметров в метод в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,48 +2,25 @@
 export default {
 	data() {
 		return {
-			num1: 1,
-		num2: 2,
+      num2: 2,
+      num3: 3,
 		}
 },
 methods: {
 	show: function() {
-    const current = new Date();
-    const dayOfWeekNumber = current.getDay();
-      let dayOfWeek;
-      switch (dayOfWeekNumber) {
-        case 0:
-          dayOfWeek = "Sunday";
-          break;
-        case 1:
-          dayOfWeek = "Monday";
-          break;
-        case 2:
-          dayOfWeek = "Tuesday";
-          break;
-        case 3:
-          dayOfWeek = "Wednesday";
-          break;
-        case 4:
-          dayOfWeek = "Thursday";
-          break;
-        case 5:
-          dayOfWeek = "Friday";
-          break;
-        case 6:
-          dayOfWeek = "Saturday";
-          break;
-        default:
-          dayOfWeek = "Something went wrong"
-          break;
-      }
-      alert(`Day of week by number = ${dayOfWeekNumber}. Day of week = ${dayOfWeek}`);
-	}
-  
+    alert(this.num2 * 2); 
+
+     
+	},
+  bob: function(){
+    alert(this.num3 * 3); 
+  }
 }
 }
 </script>
 <template>
-{{ show () }}
+
+<button @click="show">btn1</button>
+	<button @click="bob">btn2</button>
 </template>
 

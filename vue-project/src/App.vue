1856a1056fc3127131a1a287c8c893ac89b938ab
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-07 20:04:17 +0300

    Выполнения кода в представлениях Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,13 +2,16 @@
 export default {
 	data() {
 		return {
-			text: 'page',
-		href: '/',
+			num: 5,
+      num1: 1,
+		num2: 2,
+		num3: 3,
 		}
 	}
 }
 </script>
 <template>
-	<a :href=href>{{text}}</a>
+	{{ num * num }} <br>
+  {{ num1 + num2 + num3 }}
 </template>
 

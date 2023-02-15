ca21a723f85c37a9383c67b20af123495430c8d9
BusterKeatonjr <evgexa0007@gmail.com>
2023-02-15 16:08:39 +0300

    Тогглинг элементов в Vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,24 +1,32 @@
 <script>
 export default {
-	data() {
-		return {
-
-     visible: false, 
-     
-		}
-},
-methods: {
-	
-	hide: function() {
-		this.visible = true;
-	}
+  data() {
+    return {
+      visibility_p1: false,
+      visibility_p2: false,
+      visibility_p3: false,
+    }
+  },
+  methods: {
+      toggle_p1: function() {
+        this.visibility_p1 = !this.visibility_p1;
+      },
+      toggle_p2: function() {
+        this.visibility_p2 = !this.visibility_p2;
+      },
+      toggle_p3: function() {
+        this.visibility_p3 = !this.visibility_p3;
+      },
+  }
 }
-}
-
-
 </script>
 <template>
-	<button @click="hide">hide</button>
-	<p v-if="visible">text</p>
+  <button @click="toggle_p1">toggle</button><br>
+  <p v-if="visibility_p1">text p1</p>
+  <button @click="toggle_p2">toggle</button><br>
+  <p v-if="visibility_p2">text p2</p>
+  <button @click="toggle_p3">toggle</button><br>
+  <p v-if="visibility_p3">text p3</p>
+  
 </template>
 

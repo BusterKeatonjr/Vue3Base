90371990eb4f67615be6f0ac1bb58e483eae816d
BusterKeatonjr <evgexa0007@gmail.com>
2023-03-01 09:54:37 +0300

    55

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,28 +1,45 @@
 <script>
-export default {
-  data() {
-    return {
-      newItem: "",
-      items: ["a", "b", "c", "d", "e"],
-    };
-  },
-  methods: {
-    addItemEnd: function () {
-      this.items.push(this.newItem);
-    },
-    addItemBegin: function () {
-      this.items.unshift(this.newItem);
-    },
-  },
-};
+export default{
+data(){
+  return{
+    users: [
+      {
+        id: 1,
+        name: 'name1',
+        salary: 100,
+        age: 12,
+      },
+      {
+        id: 2,
+        name: 'name2',
+        salary: 200,
+        age: 12,
+      },
+      {
+        id: 3,
+        name: 'name3',
+        salary: 300,
+        age: 12,
+      },
+    ],
+  }
+},
+methods: {
+  removeUser: function (id) {
+    this.users = this.users.filter((user) => {
+      return user.id !== id;
+    })
+  }
+}
+}
 </script>
+
 <template>
-  <input v-model="newItem" />
-  <button @click="addItemEnd">add to the end</button>
-  <button @click="addItemBegin">add to the beginning</button>
-  <ul v-for="(item, index) in items" :key="index">
-    <li>
-      {{ item }}
-    </li>
-  </ul>
+<table class="user">
+  <tr v-for="user in users" :key="user.id"> {{ user.name }} {{ user.salary }} {{ user.age }} <button class="button" @click="removeUser(users.id)">remove</button>
+</tr>
+</table>
 </template>
+
+<style scoped>
+</style>

d8299b56d8108c33a6f07794f5676673954f8cea
BusterKeatonjr <evgexa0007@gmail.com>
2023-03-01 10:10:07 +0300

    63

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,9 +3,26 @@ import User from './components/Employee.vue'
 export default {
   data() {
     return {
-      name: 'Jujuba',
-      salary: '125000',
-      age: '19',
+      users: [
+        {
+          id: 1,
+          name: 'Jujuba',
+          salary: 125000,
+          age: 19
+        },
+        {
+          id: 2,
+          name: 'Capybara',
+          salary: 125000,
+          age: 18
+        },
+        {
+          id: 3,
+          name: 'Nix',
+          salary: 33000,
+          age: 0
+        },
+      ],
     }
   },
   components: {
@@ -15,7 +32,7 @@ export default {
 </script>
 
 <template>
-<User :name="name" :salary="salary" :age="age"/>
+<User v-for="user in users" :name="user.name" :salary="user.salary" :age="user.age" :key="user.id" />
 </template>
 
 
